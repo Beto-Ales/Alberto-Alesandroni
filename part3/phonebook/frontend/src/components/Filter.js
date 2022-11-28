ba9dfed9c894bd2c5098830b04d8ca4217f4b3ef
ALBERTO GERMAN ALESANDRONI <65110748+Beto-Ales@users.noreply.github.com>
2022-11-28 12:28:32 +0100

    Add files via upload

---
+++ part3/phonebook/frontend/src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Filter = ({handleFilterChange}) => {
+
+    return(
+        
+        <div>
+            Filter shown with: <input onChange={handleFilterChange} />
+        </div>
+        
+    )
+}
+
+export default Filter
